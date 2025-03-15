fd915e478d3573065b3a1b3343c114f258085986
dhyan14 <dhyanjain341988@gmail.com>
2025-03-15 19:34:37 +0530

    ss

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-const server = require('./server');
-const PORT = process.env.PORT || 3000;
-
-// Check if server is an Express app or HTTP server
-if (server.listen) {
-  server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-} else {
-  // If it's an Express app, create HTTP server
-  const http = require('http');
-  const httpServer = http.createServer(server);
-  httpServer.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
 const app = require('./server.js');
 const PORT = process.env.PORT || 5000;
 
