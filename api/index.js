f3a64701ad9ba6c9720ebebae0b80a0f29e97c84
dhyan14 <dhyanjain341988@gmail.com>
2025-03-16 09:54:03 +0530

    aa

--- api/index.js
+++ api/index.js
@@ -1,3 +1,12 @@
-const app = require('../server');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel!');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from API!' });
+});
 
 module.exports = app; 
